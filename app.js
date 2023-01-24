bd38d8b3f930d73790200ea2c167e6219cd8ad59
Nasimkrm <nassim.karimi.nk@gmail.com>
2023-01-24 16:49:34 +0100

    Add todo list app in projects section

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const express = require('express')
-const app = express()
-app.use(express.static('public'));
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
 
-
-app.get('/', (req, res) => {
-  res.sendFile("cv.html", {root:__dirname})
+app.get("/", (req, res) => {
+  res.sendFile("cv.html", { root: __dirname });
 });
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Example app listening on`+port)
+  console.log(`Example app listening on ` + port);
 });
