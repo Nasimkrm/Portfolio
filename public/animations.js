cab9c5f275ea71596be9434bd79ec4b1078db688
Nasimkrm <nassim.karimi.nk@gmail.com>
2023-02-07 14:12:46 +0100

    Some changes to styles.css

--- public/animations.js
+++ public/animations.js
@@ -1,33 +1,24 @@
+$(document).ready(function () {
+  $(".burger-icon").on("click", function () {
+    $("header ul").toggleClass("open");
 
+    return false;
+  });
 
-$(document).ready(function(){
-    
-    $(".burger-icon").on("click",function(){
-        $("header ul").toggleClass("open");
-        
-        return false
-    });
+  $("#blog").on("click", function () {
+    $("#pop-up").slideDown();
+    setTimeout(closePop, 5000);
 
-    $("#blog").on("click",function(){
-        $("#pop-up").slideDown();
-        setTimeout(closePop,5000);
-        
-        return false
-    });
+    return false;
+  });
 
-    $("#pop-up").on("click",function(){
-        closePop();
-    });
-
+  $("#pop-up").on("click", function () {
+    closePop();
+  });
 });
 
-// addClass on blog click
-//call a timer function rmvs after 5s
-//on pop up add event listener so when clicked remove class
-//
+function closePop() {
+  $("#pop-up").fadeOut();
 
-function closePop(){
-    $("#pop-up").fadeOut();
-        
-        return false
+  return false;
 }
